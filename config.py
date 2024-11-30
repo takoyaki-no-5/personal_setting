d091daff86adf8fa5b4bd0ede3df6d368788ee60
takoyaki <147268921+takoyaki4th@users.noreply.github.com>
2024-11-30 19:54:23 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,34 +2,44 @@ import re
 from xkeysnail.transform import *
 
 define_modmap({
-        Key.HENKAN:Key.ENTER,
-        Key.Q:Key.TAB,
-        Key.KATAKANAHIRAGANA:Key.CAPSLOCK,
-        Key.CAPSLOCK:Key.Q,
+	Key.KATAKANAHIRAGANA:Key.CAPSLOCK,
+	Key.CAPSLOCK:Key.ESC,
+	Key.YEN:Key.EQUAL,
+	Key.EQUAL:Key.YEN,
+	Key.LEFT_SHIFT:Key.RO,	
 })
 
 define_multipurpose_modmap({
-    Key.CAPSLOCK: [Key.Q, Key.LEFT_CTRL],  #### Caps Lockを単押しでESC,併用でCtrl###
-    Key.SPACE:[Key.SPACE,Key.LEFT_SHIFT],
-    Key.MUHENKAN:[Key.MUHENKAN,Key.LEFT_CTRL],
+    Key.SPACE:[Key.SPACE,Key.LEFT_CTRL],
+    Key.MUHENKAN:[Key.MUHENKAN,Key.LEFT_SHIFT],
+    Key.HENKAN:[Key.ENTER,Key.RIGHT_META]
 })
 
 define_keymap(None, {
-        K("C-f"): K("backspace"),
-        K("C-Shift-f"):K("backspace"),
-        K("C-d"):K("delete"),
-        K("C-m"): K("enter"),
-        K("C-l"): Key.RIGHT,
-        K("C-h"): Key.LEFT,
-        K("C-k"): Key.UP,
-        K("C-j"): Key.DOWN,
-        K("C-Shift-l"): K("Shift-right"),
-        K("C-Shift-h"): K("Shift-left"),
-        K("C-Shift-k"): K("Shift-up"),
-        K("C-Shift-j"): K("Shift-down"),
-        K("M-b"): with_mark(K("C-left")),
-        K("M-f"): with_mark(K("C-right")),#Alt+f で Ctrl+右
-        K("C-e"): with_mark(K("end")),      #Ctrl+e で 行末
-        K("j"):{K("j"):Key.ESC},
+	K("C-f"): K("backspace"),          
+	K("C-Shift-f"):K("backspace"),
+	K("C-d"):K("delete"),
+	K("C-l"): Key.RIGHT,       
+	K("C-h"): Key.LEFT,         
+	K("C-k"): Key.UP,           
+	K("C-j"): Key.DOWN,         
+	K("C-Shift-l"): K("Shift-right"),
+	K("C-Shift-h"): K("Shift-left"),
+	K("C-Shift-k"): K("Shift-up"),
+	K("C-Shift-j"): K("Shift-down"),
+	K("M-b"): with_mark(K("C-left")), 
+	K("M-f"): with_mark(K("C-right")),
+	K("C-e"): with_mark(K("end")),
+        K("RSuper-semicolon"): K("volumedown"),
+        K("RSuper-apostrophe"): K("volumeup"),
 }, "Custom Keymap for Ctrl+H and CapsLock swap")
 
+define_keymap(lambda wm_class:wm_class == "Google-chrome",{
+	K("C-s"):K("C-l"),
+	K("C-p"):K("C-w"),
+	K("C-y"):K("Shift-f10"),
+},"Chrome only")
+
+define_keymap(lambda wm_class:wm_class == "Xfce4-terminal",{
+	K("C-s"):K("C-l"),
+},"terminal only")
